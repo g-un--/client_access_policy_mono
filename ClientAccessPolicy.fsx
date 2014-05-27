d79122dfb79ae13bad95909ca3cf519eff91dbad
g-un-- <office.gino@gmail.com>
2014-05-27 03:11:13 +0300

    ClientAccessPolicy

---
+++ ClientAccessPolicy.fsx
@@ -0,0 +1,45 @@
+open System.IO
+open System.Threading
+open System.Net
+open System.Net.Sockets
+
+[<Literal>]
+let PolicyRequest = "<policy-file-request/>"
+
+[<Literal>]
+let PolicyFileResponse = "ClientAccessPolicy.xml"
+
+let read_request ( clientReader : StreamReader ) = async { 
+        let charArray : char array = Array.zeroCreate 32
+        let! bytesRead = clientReader.ReadAsync(charArray, 0, 32) |> Async.AwaitTask
+        let requestString = new string(charArray, 0, bytesRead)
+        return requestString 
+}
+
+let policy_server sourceIp = 
+    let policy = File.ReadAllText("ClientAccessPolicy.xml")
+    let server = new TcpListener(IPAddress.Parse(sourceIp), 943)
+    server.Start()
+    printfn "server started on ip %s" sourceIp
+    while true do
+        let client = server.AcceptTcpClient()
+        printfn "client connected"
+        let policyAsync = async {
+            let clientStream = client.GetStream()
+            use clientReader = new StreamReader(clientStream)
+            let! request = read_request(clientReader)
+            printfn "%s" request
+            match request = PolicyRequest with
+            | true -> 
+                printfn "ma-ta are cratima"
+                use writer = new StreamWriter(client.GetStream())
+                writer.AutoFlush <- true
+                do! writer.WriteAsync(policy).ContinueWith(fun t-> ()) |> Async.AwaitTask
+                printfn "client policy sent."
+            | _ -> printfn "wrong policy request" 
+            printfn "closing client..."
+            client.Close()
+        }
+        policyAsync |> Async.Start
+
+policy_server fsi.CommandLineArgs.[1]
